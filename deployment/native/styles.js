ae95cd7f50b65c11d3e1a47abba2cbfc598a1b82
Harshita Raj <harshita.r@comakeit.com>
2022-12-23 14:54:10 +0530

    to run github actions

---
+++ deployment/native/styles.js
@@ -0,0 +1,13 @@
+import * as style0 from "C:/Users/harshitar/Documents/Mendix/GoogleOIDC-main/themesource/administration/native/main";
+import * as style1 from "C:/Users/harshitar/Documents/Mendix/GoogleOIDC-main/themesource/webactions/native/main";
+import * as style2 from "C:/Users/harshitar/Documents/Mendix/GoogleOIDC-main/themesource/atlas_core/native/main";
+import * as style3 from "C:/Users/harshitar/Documents/Mendix/GoogleOIDC-main/themesource/myfirstmodule/native/main";
+import * as style4 from "C:/Users/harshitar/Documents/Mendix/GoogleOIDC-main/themesource/nanoflowcommons/native/main";
+import * as style5 from "C:/Users/harshitar/Documents/Mendix/GoogleOIDC-main/themesource/oidc/native/main";
+import * as style6 from "C:/Users/harshitar/Documents/Mendix/GoogleOIDC-main/themesource/communitycommons/native/main";
+import * as style7 from "C:/Users/harshitar/Documents/Mendix/GoogleOIDC-main/themesource/nativemobileresources/native/main";
+import * as style8 from "C:/Users/harshitar/Documents/Mendix/GoogleOIDC-main/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7, style8]);
---
+++ deployment/tmp/jsactions.js
@@ -0,0 +1,1 @@
+export const NanoflowCommons$OpenURL = async () => (await import("C:/Users/harshitar/Documents/Mendix/GoogleOIDC-main/javascriptsource/nanoflowcommons/actions/OpenURL")).OpenURL;
